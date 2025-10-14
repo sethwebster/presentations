885ebb77c5c5032173b315cae7522495c5fef455
sethwebster <sethwebster@gmail.com>
2025-10-14 02:09:16 -0400

    Add missing useCallback import in useAutopilot

--- presentation-framework/src/autopilot/useAutopilot.js
+++ presentation-framework/src/autopilot/useAutopilot.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useCallback } from 'react';
 import { useRealtimeSpeech } from './useRealtimeSpeech';
 import { useAutoAdvance } from './useAutoAdvance';
 import { extractSpeakerNotes } from './extractSpeakerNotes';
